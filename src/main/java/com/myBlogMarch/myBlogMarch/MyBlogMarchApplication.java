9bf83c9962aff7c36400fe0d0aecf71418b96ddc
varsha yadav <varshayadav13077@gmail.com>
2024-03-27 11:13:33 +0530

    127: create save post feature

---
+++ src/main/java/com/myBlogMarch/myBlogMarch/MyBlogMarchApplication.java
@@ -0,0 +1,13 @@
+package com.myBlogMarch.myBlogMarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogMarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogMarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myBlogMarch/myBlogMarch/MyBlogMarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBlogMarch.myBlogMarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogMarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
